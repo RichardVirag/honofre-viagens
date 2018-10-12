b412ecac9eb607120b07e91f906988b87925a35e
Richard Virag <andraderv10@gmail.com>
2018-10-12 16:20:57 -0300

    Estrutura inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CategoryComponent } from './category/category.component';
+import { BannerComponent } from './banner/banner.component';
+import { PackageComponent } from './package/package.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    CategoryComponent,
+    BannerComponent,
+    PackageComponent
   ],
   imports: [
     BrowserModule,
