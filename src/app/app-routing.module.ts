31613794c2d485dc193110b81d42934a2ab1a401
Richard Virag <andraderv10@gmail.com>
2018-10-14 16:28:57 -0300

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BannerComponent } from './banner/banner.component';
+import { CategoryComponent } from './category/category.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PackageComponent } from './package/package.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+       path: '',
+       component: DashboardComponent
+    },
+    {
+       path: 'banners',
+       component: BannerComponent
+    },
+    {
+       path: 'categorias',
+       component: CategoryComponent
+    },
+    {
+       path: 'pacotes',
+       component: PackageComponent
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
